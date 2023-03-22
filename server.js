1f0604a5cbe01c5858d3fe2965dfc90fcf1cf8da
Nsher00 <jimmysherwood9@gmail.com>
2023-03-22 17:03:31 -0600

    made changes to the express.static function

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(express.static(`${--dirname}/public`))
+app.use(express.static(`${__dirname}/public`))
 
 app.listen(4000, ()=>{
     console.log('app in up on port 4000');
